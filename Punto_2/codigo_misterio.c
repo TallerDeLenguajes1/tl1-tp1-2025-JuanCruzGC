85dc68a132463bc73467a789c452d0d7d704c5b0
Juan Cruz <juangomezcorbalan@gmail.com>
2026-04-05 13:22:11 -0300

    ejercicio 3 completado

--- Punto_2/codigo_misterio.c
+++ Punto_2/codigo_misterio.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void f_alpha(int *p) {
+void invertir(int *p) {
     int temp = *p;
     int rev = 0;
     while (temp > 0) {
@@ -10,11 +10,11 @@ void f_alpha(int *p) {
     *p = rev;
 }
 
-void f_beta(int *p) {
+void divideDos(int *p) {
     *p = *p / 2;
 }
 
-void f_gamma(int *p) {
+void sumaDigitos(int *p) {
     int temp = *p;
     int suma = 0;
     while (temp > 0) {
@@ -24,22 +24,22 @@ void f_gamma(int *p) {
     *p = *p + suma;
 }
 
-void procesar_enigma(int *valor_referencia) {
-    f_alpha(valor_referencia);
-    f_beta(valor_referencia);
-    f_gamma(valor_referencia);
+void procesar_numero(int *valor_referencia) {
+    invertir(valor_referencia);
+    divideDos(valor_referencia);
+    sumaDigitos(valor_referencia);
 }
 
 int main() {
-    int dato_secreto = 452;
+    int num = 452;
     
-    printf("Iniciando depuracion con el valor: %d\n", dato_secreto);
+    printf("Iniciando depuracion con el valor: %d\n", num);
     
     // Instrucción para el alumno: 
     // Pon un breakpoint aquí (F9) y usa F11 (Step Into) para entrar a cada función.
-    procesar_enigma(&dato_secreto);
+    procesar_numero(&num);
     
-    printf("Resultado final del enigma: %d\n", dato_secreto);
+    printf("Resultado final del enigma: %d\n", num);
     
     return 0;
 }
