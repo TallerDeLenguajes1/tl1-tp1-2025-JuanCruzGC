729b4afe35671e45ee500a66b1b60b872fb2001a
Juan Cruz <juangomezcorbalan@gmail.com>
2026-04-06 10:59:02 -0300

    agrego .gitignore y modifico tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo");
+    int *puntero;
+    int apuntado=22;
+    puntero=&apuntado;
+    printf("Contenido del puntero: %d\n",*puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p\n",puntero);
+    printf("Direccion de memoria de la variable: %p\n",&apuntado);
+    printf("Direccion de memoria del puntero: %p\n",&puntero);
+    printf("Tamanio de memoria utilizado por apuntado: %zu bytes\n",sizeof(apuntado));
+    printf("Tamanio de memoria utilizado por puntero: %zu bytes",sizeof(puntero));
     return 0;
 }
